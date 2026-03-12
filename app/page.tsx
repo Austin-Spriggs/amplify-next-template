22a9f7456930e6a12a790bee1e469f3e47302c96
Austin Spriggs <aspriggs@madisoncollege.edu>
2026-03-12 15:44:19 -0500

    moved todos into their own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,14 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
+import { ToDo } from "./components/ToDo";
 
 export default function App() {
-	const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-	function listTodos() {
-		client.models.Todo.observeQuery().subscribe({
-			next: (data) => setTodos([...data.items]),
-		});
-	}
-
-	useEffect(() => {
-		listTodos();
-	}, []);
-
-	function createTodo() {
-		client.models.Todo.create({
-			content: window.prompt("Todo content"),
-		});
-	}
-
-
-	function deleteTodo(id: string) {
-		client.models.Todo.delete({ id })
-	}
-
 	return (
 		<main>
 			<h1>My todos</h1>
-			<button onClick={createTodo}>+ new</button>
-			<ul>
-				{todos.map((todo) => (
-					<li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
-				))}
-			</ul>
+
+			<ToDo />
+
 			<div>
 				🥳 App successfully hosted. Try creating a new todo.
 				<br />
